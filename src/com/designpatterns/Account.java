4fa1e28e5346eaba6db9a7af2ed1634cb4ed9b0c
tasnuvaleeya <tasnuvaleeya@gmail.com>
2020-07-08 15:36:06 +0600

    Working with Coupling, Interfaces, Encapsulation

---
+++ src/com/designpatterns/Account.java
@@ -0,0 +1,18 @@
+package com.designpatterns;
+
+public class Account {
+    private float balance;
+
+    public void deposit(float amount){
+        if (amount > 0)
+        balance += amount;
+    }
+    public void withdraw(float amount){
+        if (amount > 0)
+            balance -= amount;
+    }
+
+    public float getBalance(){
+        return balance;
+    }
+}
--- src/com/designpatterns/Main.java
+++ src/com/designpatterns/Main.java
@@ -3,17 +3,11 @@ package com.designpatterns;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        User user = new User("Tasnuva", 28);
-//        System.out.println(user.name);
-//        user.sayHello();
-
-        TaxCalculator calculator = getCalculator();
-        calculator.calculateTax();
+        var account = new Account();
+        account.deposit(10);
+        account.withdraw(5);
+        System.out.println(account.getBalance());
 
     }
 
-    public static TaxCalculator getCalculator(){
-        return new TaxCalculator2019();
-    }
 }
