0294d333900d69f96a7dfd6515879163817f49d7
tasnuvaleeya <tasnuvaleeya@gmail.com>
2020-07-09 18:20:59 +0600

    Working with Memento pattern

--- src/com/designpatterns/Main.java
+++ src/com/designpatterns/Main.java
@@ -1,12 +1,23 @@
 package com.designpatterns;
 
+import com.designpatterns.memento.Editor;
+import com.designpatterns.memento.History;
+
 public class Main {
     public static void main(String[] args) {
-        drawUIControl(new CheckBox());
+        var editor = new Editor();
+        var history = new History();
+        editor.setContent("a");
+        history.push(editor.createState());
+        editor.setContent("b");
+        history.push(editor.createState());
+        editor.setContent("c");
+//        editor.restore(history.pop());
+        editor.restore(history.pop());
+        System.out.println(editor.getContent());
+
+//        editor.restore(history.pop());
+
     }
 
-    public static void drawUIControl(UIControl control) {
-        control.draw();
-
-    }
 }
